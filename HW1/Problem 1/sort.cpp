3f1303df6a07f387ae914e657a87b085df7a6b3e
andrewkaldani <59716037+andrewkaldani@users.noreply.github.com>
2020-02-04 19:38:08 -0800

    Update sort.cpp

--- HW1/Problem 1/sort.cpp
+++ HW1/Problem 1/sort.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+const int SIZE = 50;
+void writeletter(string names[], int size, char letter, ostream& out);
+int main()
+{
+  ifstream in;
+  ofstream o;
+  string n[SIZE];
+  int s=0;
+
+
+
+  in.open("input.txt");
+  o.open("output.txt");
+  if(in.fail())
+  {
+    cout<<"fail"<<endl;
+    exit(1);
+  }
+  if(o.fail())
+  {
+    cout<<"fail"<<endl;
+    exit(1);
+  }
+  while (!in.eof())
+  {
+    in>>n[s];
+    s++;
+  }
+  char l[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+  for(int i =0; i<26; i++)
+    writeletter(n,s,l[i],o);
+  in.close();
+  o.close();
+  return 0;
+}
+void writeletter(string names[], int size, char letter, ostream& out)
+{
+  for(int i = 0; i < size; i++){
+    if(names[i][0] == letter)
+      out<<names[i]<<endl;
+  }
+
+
+}
