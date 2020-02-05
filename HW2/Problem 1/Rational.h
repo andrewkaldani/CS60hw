bccf8e92b5e493a14cc95ea47032eb0bf62cc849
andrewkaldani <59716037+andrewkaldani@users.noreply.github.com>
2020-02-04 19:48:46 -0800

    Create Rational.h

---
+++ HW2/Problem 1/Rational.h
@@ -0,0 +1,43 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#include <string>//ADT Style:  no using namespace std;
+#include <iostream>
+class Rational{
+
+public:
+    Rational(int n, int d);
+    Rational();
+    int numer(){  return numer_; }//ADT Style:give the getter the variable name minus the _
+    int denom(){  return denom_; }//ADT Style:Don't bother with forward declaration for one-liner
+
+
+
+
+    void operator *=(Rational b);
+    //pre: none
+    //post: will simplify the fraction after you add and multiply
+    void reduce();
+    //pre none
+    //post: modifies the rational called on adds the first and second fraction together
+    void operator +=(Rational rhs);
+      private:
+      int numer_;//ADT Style: trailing _ means this is a private variable
+      int denom_;
+};
+//pre: none
+//post: returns a Rational that is the result of multiplying this Rational and the Rational passed in.
+//The result is not reduced; the denominator will be the product of the denominators of the operands.
+Rational operator *(Rational a, Rational b);
+
+//pre: none
+//post: returns a Rational that is the result of adding this Rational and the Rational passed in.
+//The result is not reduced; the denominator will be the product of the denominators of the operands.
+Rational operator +(Rational a, Rational b);
+std::ostream& operator <<(std::ostream& out, Rational a);
+//The problem was that I was missing the std:: in front of ostream
+// pre: none
+//post: returns if two fractions are equal to other or not
+ bool operator ==(Rational a, Rational b);
+
+#endif
